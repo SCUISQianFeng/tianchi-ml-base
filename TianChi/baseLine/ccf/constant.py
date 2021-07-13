92fd806b0ee5f3dd4b989810a53c9a42e0961d69
lishuai-1 <lishuai-1@thunisoft.com>
2021-07-13 18:02:45 +0800

    o2o

---
+++ TianChi/baseLine/ccf/constant.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+"""
+    常量
+"""
+
+
+class _const:
+    class ConstError(TypeError):
+        pass
+
+    class ConstCaseError(ConstError):
+        pass
+
+    def __setattr__(self, key, value):
+        if key in self.__dict__:
+            raise self.ConstError("Can't change const.{}".format(key))
+        if not key.isupper():
+            raise self.ConstCaseError("const name {} is not all uppercase".format(key))
+        self.__dict__[key] = value
+
+
+import sys, os
+
+sys.path.append(os.pardir)
+# sys.path.append('..')
+
+# sys.modules[__name__] == _const()
+const = _const()
+
+const.MY_CONSTANT = 1
+const.MY_SECOND_CONSTANT = 2
---
+++ TianChi/baseLine/ccf/main.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+    main 程序启动入口
+"""
+import sys, os
+from constant import const
+
+sys.path.append(os.pardir)
+
+if __name__ == "__main__":
+
+    print(const.MY_CONSTANT)
