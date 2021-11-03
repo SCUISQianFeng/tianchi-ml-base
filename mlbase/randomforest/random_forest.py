5fed0e6828520c4e2fd02c82cb7361244498b30d
Lis <scuislishuai@foxmail.com>
2021-11-03 23:35:24 +0800

    forest init code

--- mlbase/randomforest/random_forest.py
+++ mlbase/randomforest/random_forest.py
@@ -1,9 +1,22 @@
-import numpy as np
-from collections import Counter
+# coding:utf8
 
 import sys
+from random import randint
+from random import randrange
+from collections import Counter
+
+import numpy as np
 
 sys.path.append('..')
+# hyper parameter
+# fold size (% of dataset size) e.g. 3 means 30%
+FOLD_SIZE = 10
+# number of trees
+N_TREES = 20
+# max tree depth
+MAX_DEPTH = 30
+# min size of tree node
+MIN_NODE = 1
 
 
 class DecisionTree:
@@ -172,16 +185,98 @@ class DecisionTree:
         return np.array(y_predict)
 
 
-if __name__ == "__main__":
-    # # test dataset
-    # X = np.array([[1, 1,0], [3, 1, 0], [1, 4, 0], [2, 4, 1], [3, 3, 1], [5, 1, 1]])
-    # y = np.array([0, 0, 0, 1, 1, 1])
+class RandomForest:
+    def __init__(self, n_trees, fold_size):
+        self.n_trees = n_trees
+        self.fold_size = fold_size
+        self.trees = list()
 
+    def cross_validation_split(self, dataset, n_folds, p):
+        """
+        This function splits the given dataset into n-folds with replacement
+        :param dataset:  np array of the given dataset
+        :param n_folds: number of folds in which the dataset should be split. Must be equal to the number of trees the user wants to train
+        :param p: suggests the percentage of the dataset's size the size of a single fold should be.
+        :return: list with the k-folds
+        """
+        dataset_split = list()
+        fold_size = int(len(dataset) * p / 10)
+        for _ in range(n_folds):
+            fold = list()
+            while len(fold) < fold_size:
+                index = randrange(len(dataset))
+                fold.append(dataset[index])
+            fold_set = np.array(fold)
+            dataset_split.append(fold_set)
+        return dataset_split
+
+    def random_features(self, splits):
+        """
+        randomizes the selection of the features each tree will be trained on.
+        :param splits: list of folds
+        :return: list with the k-folds with some features randomly removed
+        """
+        dataset_split = list()
+        l = len(splits[0][0])  # 第一个分组的第一个元素的长度就是特征的数量
+        n_features = int((l - 1) * 5 / 10)  # 随机选择一半数量的特征
+        for split in splits:
+            for i in range(n_features):  # 随机删除一个特征
+                rng = list(range(len(split[0]) - 1))
+                selected = rng.pop(randint(0, len(rng) - 1))
+                split = np.delete(split, selected, 1)
+            set = np.array(split)
+            dataset_split.append(set)  # 每个分组的数据都随机删除一半的特征
+        return dataset_split
+
+    def print_tree(self):
+        i = 1
+        for t in self.trees:
+            print("Tree#", i)
+            temp = t.final_tree
+            t.print_dt(temp)
+            print("\n")
+            i = i + 1
+
+    def train(self, X):
+        train_x = self.cross_validation_split(X, self.n_trees, self.fold_size)
+        train_x = self.random_features(train_x)
+        for fold in train_x:
+            dt = DecisionTree(MAX_DEPTH, MIN_NODE)
+            dt.train(fold)
+            self.trees.append(dt)
+
+    def predict(self, X):
+        predicts = list()
+        final_predicts = list()
+        for tree in self.trees:
+            predicts.append(tree.predict(X))
+        for i in range(len(predicts[0])):
+            values = list()
+            for j in range(len(predicts)):
+                values.append(predicts[j][i])
+            final_predicts.append(max(set(values), key=values.count))
+        return final_predicts, predicts
+
+
+if __name__ == "__main__":
+
+    # Training data
     train_data = np.loadtxt("example_data/data.txt", delimiter=",")
     train_y = np.loadtxt("example_data/targets.txt")
 
-    # Build tree
-    dt = DecisionTree(5, 1)
-    tree = dt.train(train_data)
-    y_pred = dt.predict(train_data)
-    print(f"Accuracy: {sum(y_pred == train_y) / train_y.shape[0]}")
+    mock_train = np.loadtxt("example_data/mock_data.csv", delimiter=",")
+    mock_y = mock_train[:, -1]
+
+    # Build and train model
+    rf = RandomForest(N_TREES, FOLD_SIZE)
+    rf.train(mock_train)
+
+    # Evaluate model on training data
+    y_pred, y_pred_ind = rf.predict(mock_train)
+    print(f"Accuracy of random forest: {sum(y_pred == mock_y) / mock_y.shape[0]}")
+    print("\nAccuracy for each individual tree:")
+    c = 1
+    for i in y_pred_ind:
+        print("\nTree", c)
+        print(f"Accuracy: {sum(i == mock_y) / mock_y.shape[0]}")
+        c = c + 1
